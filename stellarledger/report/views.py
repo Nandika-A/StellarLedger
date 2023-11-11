a7a89aa5089c20c3972a6aac7c374801b986dd2c
Nandika-A <nandikaagrawal610@gmail.com>
2023-11-11 20:04:17 +0530

    transaction functions

--- stellarledger/report/views.py
+++ stellarledger/report/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def recordTransaction(request):
+    pass
+
+def createCategory(request):
+    pass
+
+def deleteCategory(request):
+    pass
+
+def updateTransaction(request):
+    pass
+
+def deleteTransaction(request):
+    pass
+
+def viewTransactions(request):
+    pass
+
+def getExpenses():
+    pass
+
+def getSavings():
+    pass
+
+def getExpenditure():
+    pass
+
+def changeTransactionCategory():
+    pass
+
+def recurringbills():
+    pass
