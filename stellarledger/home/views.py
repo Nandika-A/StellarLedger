a288a56b3d5b0c3d44d4871a5135504cf10d574e
Nandika-A <nandikaagrawal610@gmail.com>
2023-11-24 16:39:58 +0530

    added designs

--- stellarledger/home/views.py
+++ stellarledger/home/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from report.models import Transaction, Category
+from report.views import getExpenses, getSavings
 # Create your views here.
 
-def homepage(request):      
-    return render(request, 'home/homepage.html')
+def homepage(request): 
+    total_expenses = []
+    savings = []
+    cat = Category.objects.filter(user=request.user)
+    for c in cat:
+        txn = Transaction.objects.filter(user=request.user, category=c.category)
+        e=getExpenses(txn)
+        s=getSavings(txn)
+        if s < 0:
+            s = 0 
+
+        total_expenses.append(e)
+        savings.append(s)
+         
+    return render(request, 'home/homepage.html',{
+        'expenses': total_expenses,
+        'cat': cat,
+        'savings': savings
+    })
