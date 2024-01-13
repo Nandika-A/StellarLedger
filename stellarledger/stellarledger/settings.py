9494cff21fa119afb27f392e19f00d9f85510264
Nandika-A <nandikaagrawal610@gmail.com>
2024-01-13 16:02:47 +0530

    Changed design

--- stellarledger/stellarledger/settings.py
+++ stellarledger/stellarledger/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+srt$$2+$uy_ms%^ly#8dpbp(mr*m5x@&j40_4**bzm^k@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
